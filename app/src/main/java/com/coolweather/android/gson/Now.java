02e1d31be30d3473906a150693d5ac080f4c16ff
cjh <564683361@qq.com>
2018-07-21 16:58:13 +0800

    完成天气界面上显示天气信息功能

--- app/src/main/java/com/coolweather/android/gson/Now.java
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -9,6 +9,7 @@ import com.google.gson.annotations.SerializedName;
 public class Now {
     @SerializedName("tmp")
     public String temperature;
+    @SerializedName("cond")
     public More more;
     public class More{
         @SerializedName("txt")
